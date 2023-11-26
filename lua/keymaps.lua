e8fd3643e9fcc018494d349498ced66f5a08b2a8
Calvin Farias <calvinfarias@users.noreply.github.com>
2023-11-26 14:53:36 -0300

    Add missing features

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,13 +5,14 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
-
 -- Set leader
 vim.g.mapleader = "\\"
 vim.g.maplocalleader = "\\"
 
-
+-- nope
 keymap("", "<Space>", "<Nop>", opts)
+
+-- shortcut for saving files
 keymap("n", "<leader>w", ":w!<CR>", opts)
 
 -- navigate buffers
@@ -19,6 +20,13 @@ keymap("n", "<tab>", ":bnext<CR>", opts) 		-- Next Tab
 keymap("n", "<s-tab>", ":bprevious<CR>", opts) 		-- Previous tab
 keymap("n", "<leader>h", ":nohlsearch<CR>", opts) 	-- No highlight search
 
+-- navigation
+keymap("n", "<C-j>", "<C-y>", opts)
+keymap("n", "<C-k>", "<C-e>", opts)
+keymap("n", "<C-h>", "b", opts)
+keymap("n", "<C-l>", "w", opts)
+keymap("n", "<leader>h", "<Home>", opts)
+keymap("n", "<leader>l", "<End>", opts)
 
 -- move text up and down
 keymap("n", "<a-k>", ":m .-2<CR>==", opts) 	-- Alt-k
@@ -35,6 +43,8 @@ keymap("x", "<M-up>", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "<", "<gv", opts) 				-- Right Indentation
 keymap("v", ">", ">gv", opts) 				-- Left Indentation
 
-
-
+-- replace word
 keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", { noremap = true })
+
+-- make
+keymap("n", "<leader>m", ":w!<bar> !make %:r<CR>", { noremap = true })
--- lua/options.lua
+++ lua/options.lua
@@ -1,27 +1,60 @@
 local opt = vim.opt
 
-
---
--- incremental search
+-- Incremental search
 opt.incsearch = true
+opt.hlsearch = true
 
--- show invisble characters
+-- Show invisble characters
 opt.list = true
 
--- precede each line with its line number
+opt.listchars:append({ tab = "▹ " }) -- alternatively use ▸
+-- opt.listchars:append({ eol = "¬" })
+-- opt.listchars:append({ trail = "·" })
+-- opt.listchars:append({ nbsp = "⎵" })
+
+-- Precede each line with its line number
 opt.number = true
 
--- Relative line numbers
+-- Disable relative line numbers
 opt.relativenumber = false
 
 -- Disable line wrap
 opt.wrap = false
 
--- Size of an indent
+-- Configure indention
+opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
+opt.expandtab = true
+
+-- Set column bar
+opt.colorcolumn = "120"
 
 -- Fix markdown indentation settings
 --vim.g.markdown_recommended_style = 0
---
+
+-- Backups
+opt.swapfile = false
+opt.backup = true
+opt.backupdir = ".backup/,~/.vim/backup/,/tmp//"
+
+-- For better performance
+opt.updatetime = 50
+
+vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+
+-- When opening a buffer, place the cursor in its last know position
+local group = vim.api.nvim_create_augroup("jump_last_position", { clear = true })
+vim.api.nvim_create_autocmd(
+	"BufReadPost",
+	{callback = function()
+			local row, col = unpack(vim.api.nvim_buf_get_mark(0, "\""))
+			if {row, col} ~= {0, 0} then
+				vim.api.nvim_win_set_cursor(0, {row, col})
+			end
+		end,
+	group = group
+	}
+)
 
 
